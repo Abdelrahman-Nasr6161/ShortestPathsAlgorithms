56c14a1c27ebb33bd27aaf35851727fe51a771cc
abdelrahmanmohammednasr <abdoahlawy6161@gmail.com>
2025-04-18 19:30:15 +0200

    added final cost returning

--- short/src/main/java/demo/Classes/Graph.java
+++ short/src/main/java/demo/Classes/Graph.java
@@ -17,6 +17,7 @@ public class Graph {
     public int V; // number of vertices
     public List<Edge> edges;
     public List<Integer> finalDist;
+    public List<List<Integer>> finalDistMatrix;
     public Graph(int V) {
         this.V = V;
         edges = new ArrayList<>();
